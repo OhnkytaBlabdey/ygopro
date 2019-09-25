c97580e33d660498ece2422025249a021e1682a1
OhnkytaBlabdey <1263189143@qq.com>
2019-09-25 16:42:04 +0800

    use tab

--- premake5.lua
+++ premake5.lua
@@ -1,53 +1,53 @@
 solution "ygo"
-    location "build"
-    language "C++"
-    objdir "obj"
+	location "build"
+	language "C++"
+	objdir "obj"
 
-    configurations { "Release", "Debug" }
+	configurations { "Release", "Debug" }
 
-    configuration "windows"
-        defines { "WIN32", "_WIN32" }
+	configuration "windows"
+		defines { "WIN32", "_WIN32" }
 
-    configuration "linux"
-        defines { "LUA_USE_LINUX" }
+	configuration "linux"
+		defines { "LUA_USE_LINUX" }
 
-    configuration "Release"
-        targetdir "bin/release"
+	configuration "Release"
+		targetdir "bin/release"
 
-    configuration "Debug"
-        symbols "On"
-        defines "_DEBUG"
-        targetdir "bin/debug"
+	configuration "Debug"
+		symbols "On"
+		defines "_DEBUG"
+		targetdir "bin/debug"
 
-    configuration { "Release", "vs*" }
-        optimize "Speed"
-        flags { "StaticRuntime", "LinkTimeOptimization" }
-        disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
+	configuration { "Release", "vs*" }
+		optimize "Speed"
+		flags { "StaticRuntime", "LinkTimeOptimization" }
+		disablewarnings { "4244", "4267", "4838", "4577", "4819", "4018", "4996", "4477" }
 
-    configuration { "Release", "not vs*" }
-        symbols "On"
-        defines "NDEBUG"
-        buildoptions "-march=native"
+	configuration { "Release", "not vs*" }
+		symbols "On"
+		defines "NDEBUG"
+		buildoptions "-march=native"
 
-    configuration { "Debug", "vs*" }
-        defines { "_ITERATOR_DEBUG_LEVEL=0" }
+	configuration { "Debug", "vs*" }
+		defines { "_ITERATOR_DEBUG_LEVEL=0" }
 
-    configuration "vs*"
-        flags "EnableSSE2"
-        defines { "_CRT_SECURE_NO_WARNINGS" }
+	configuration "vs*"
+		flags "EnableSSE2"
+		defines { "_CRT_SECURE_NO_WARNINGS" }
 
-    configuration "not vs*"
-        buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
+	configuration "not vs*"
+		buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
 
-    configuration {"not vs*", "windows"}
-        buildoptions { "-static-libgcc" }
+	configuration {"not vs*", "windows"}
+		buildoptions { "-static-libgcc" }
 
-    startproject "ygopro"
+	startproject "ygopro"
 
-    include "lua"
-    include "ocgcore"
-    include "gframe"
-    if os.is("windows") then
-    include "event"
-    include "sqlite3"
-    end
+	include "lua"
+	include "ocgcore"
+	include "gframe"
+	if os.is("windows") then
+	include "event"
+	include "sqlite3"
+	end
